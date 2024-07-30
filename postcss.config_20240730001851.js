93dc0e65736c1108c14cd4ff74616e898af18ab0
LongAloso <151179556+LongAloso@users.noreply.github.com>
2024-07-30 21:21:07 +0700

    Update postcss.config_20240730001851.js

--- postcss.config_20240730001851.js
+++ postcss.config_20240730001851.js
@@ -1,16 +1,1 @@
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
-// vue.config.js
-// module.exports = {
-//   publicPath: process.env.VUE_APP_BASE_URL || '/'
-// }
-// module.exports = {
-//   plugins: {
-//     tailwindcss: {},
-//     autoprefixer: {},
-//   },
-// }
+

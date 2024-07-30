329c953e0dbb900bf3e34dcc58c29507f2ff93f9
LongAloso <151179556+LongAloso@users.noreply.github.com>
2024-07-30 21:18:46 +0700

    Add files via upload

---
+++ postcss.config_20240729193227.js
@@ -0,0 +1,16 @@
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
+// vue.config.js
+module.exports = {
+  publicPath: process.env.VUE_APP_BASE_URL || '/'
+}
+// module.exports = {
+//   plugins: {
+//     tailwindcss: {},
+//     autoprefixer: {},
+//   },
+// }
